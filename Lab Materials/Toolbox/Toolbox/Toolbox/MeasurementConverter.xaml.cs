a5244a70f6c1942576ad9f1a1f99463d41b71817
Mitch Muenster <mitchmuenster@gmail.com>
2015-09-28 12:40:45 -0500

    Initial Commit of files
    These are the files so far for the Class the app its self, the word
    version of the instructions (eventually should be a .html file) and the
    powerpoint as is

---
+++ Lab Materials/Toolbox/Toolbox/Toolbox/MeasurementConverter.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Toolbox
+{
+    public partial class MeasurementConverter : ContentPage
+    {
+        public MeasurementConverter()
+        {
+            InitializeComponent();
+        }
+    }
+}
