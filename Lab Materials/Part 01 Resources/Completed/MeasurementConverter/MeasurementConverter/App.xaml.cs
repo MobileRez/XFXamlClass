fbf445ecd98a913c18edf83ba8e13c416f1966dc
Mitch Muenster <mitchmuenster@gmail.com>
2016-04-07 02:13:36 -0500

    Added support for UWP and code refactor
    Added support for Windows Universal Platform  and Windows Desktop Apps.
    Refactored code to a new name space and added app.xaml instead of app.cs
    to show how styles and the global resources work.

---
+++ Lab Materials/Part 01 Resources/Completed/MeasurementConverter/MeasurementConverter/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MeasurementConverter.View;
+using Xamarin.Forms;
+
+namespace MeasurementConverter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MunitConverter();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Lab Materials/Part 01 Resources/StartHere/MeasurementConverter/MeasurementConverter/Helper/MunitConverter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementConverter.Helper
+{
+    class MunitConverter
+    {
+        //use meters as base 
+
+        //put # from start into meters
+
+        //convert meters to ending value
+
+        //public static class DistanceRule
+        //{
+        //     public static Dictionary<DistanceUnit> 
+        //}
+
+        public enum DistanceUnits
+        {
+            Kilometer,
+            Meter,
+            Centimeter,
+            Millimeter,
+            Mile,
+            Yard,
+            Foot,
+            Inch
+        }
+
+        public static class Rule
+        {
+            public static Dictionary<DistanceUnits, double> Rules = new Dictionary<DistanceUnits, double>()
+            {
+                //The values are in respect to 1 meter. 
+                //Ex) 1 meter = 1000 millimeters
+                [DistanceUnits.Millimeter] = 1000,
+                [DistanceUnits.Centimeter] = 100,
+                [DistanceUnits.Meter] = 1,
+                [DistanceUnits.Kilometer] = .001,
+                [DistanceUnits.Inch] = 39.3701,
+                [DistanceUnits.Foot] = 3.28084,
+                [DistanceUnits.Yard] = 1.09361,
+                [DistanceUnits.Mile] = 0.000621371,
+            };
+        }
+
+        public static class UnitConverter
+        {
+            public static double Convert(double valueToConvert, DistanceUnits startUnit, DistanceUnits endUnit)
+            {
+                //value as meters
+                double meterValue = ConvertToMeter(startUnit, valueToConvert);
+                //value as final unit
+                double finalValue = ConvertMeterTo(endUnit, meterValue);
+
+                return Math.Round(finalValue, 2);
+            }
+
+            private static double ConvertToMeter(DistanceUnits origUnit, double value)
+            {
+                if (Rule.Rules.ContainsKey(origUnit))
+                {
+                    return value / Rule.Rules[origUnit];
+                }
+                else
+                {
+                    throw new Exception("Invalid unit");
+                }
+            }
+
+            private static double ConvertMeterTo(DistanceUnits destUnit, double value)
+            {
+                if (Rule.Rules.ContainsKey(destUnit))
+                {
+                    return value * Rule.Rules[destUnit];
+                }
+                else
+                {
+                    throw new Exception("Invalid unit");
+                }
+            }
+        }
+    }
+}
